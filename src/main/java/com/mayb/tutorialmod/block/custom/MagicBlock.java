e371c454dd9448074f67fe88474c87cd8a708bfc
maybMedia <braithmay@yahoo.com.au>
2024-10-22 19:18:58 +1100

    Add custom tag for transformable items

--- src/main/java/com/mayb/tutorialmod/block/custom/MagicBlock.java
+++ src/main/java/com/mayb/tutorialmod/block/custom/MagicBlock.java
@@ -1,6 +1,7 @@
 package com.mayb.tutorialmod.block.custom;
 
 import com.mayb.tutorialmod.item.ModItems;
+import com.mayb.tutorialmod.util.ModTags;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.entity.Entity;
@@ -36,12 +37,16 @@ public class MagicBlock extends Block {
     @Override
     public void onSteppedOn(World world, BlockPos pos, BlockState state, Entity entity) {
         if(entity instanceof ItemEntity itemEntity) {
-            if(itemEntity.getStack().getItem() == ModItems.RAW_PINK_GARNET){
+            if(isValidItem(itemEntity.getStack())){
                 itemEntity.setStack(new ItemStack(Items.DIAMOND, itemEntity.getStack().getCount()));
             }
         }
 
         super.onSteppedOn(world, pos, state, entity);
+    }
+
+    private boolean isValidItem(ItemStack stack) {
+        return stack.isIn(ModTags.Items.TRANSFORMABLE_ITEMS);
     }
 
     @Override
